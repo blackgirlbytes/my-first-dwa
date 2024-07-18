b59a949aab534b7e195de23b15ad3d62723833bb
Rizel Scarlett <rizel@blackgirlbytes.local>
2024-07-18 02:21:39 -0400

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
+// pages/_app.js
+import { useEffect } from 'react';
+import {activatePolyfills} from '../../polyfills';
+
+function App({ Component, pageProps }) {
+  useEffect(() => {
+    activatePolyfills({
+      // Allow service worker registration for Web5 functionality
+      onCacheCheck: (event, route) => {
+        return {
+          ttl: 60_000
+        };
+      }
+    });
+  }, []);
+
   return <Component {...pageProps} />;
 }
+
+export default App;
